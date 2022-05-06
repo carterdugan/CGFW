7cd2a0e10909c5324f7e9331d0e4aa1ad4cc09cf
Carter Dugan <cdugan@mtu.edu>
2022-05-06 18:38:47 -0400

    Added comments to pong example

--- Examples/Pong/Pong.cpp
+++ Examples/Pong/Pong.cpp
@@ -1,8 +1,13 @@
 #include "Pong.hpp"
 
+
 Pong::Pong() {
+
+	// Contructor takes an instance of a GameState subclass
 	cgfw::Game pong(this);
 
+
+	// SFML used for the players and ball
 	player1.setSize(sf::Vector2f(20,80));
 	player2.setSize(sf::Vector2f(20,80));
 	ball.setRadius(10);
@@ -15,29 +20,35 @@ Pong::Pong() {
 	ball_velocity_x = 5;
 	ball_velocity_y = 2;
 
+	// Run the game to start the loop
 	pong.run();
 }
 
 void Pong::tick(cgfw::Game* game) {
 
+	// Time step multiplier for consistent entity movement speed
+	float mult = game->getTimeStep().getMultiplier();
+
+	// Check the input handler for pressed keys
 	if(game->getInputHandler().keyPressed(sf::Keyboard::W))
-		player1.move(sf::Vector2f(0, -5 * game->getTimeStep().getMultiplier()));
+		// Multiply moving entities by the time step multiplier for consistent speed
+		player1.move(sf::Vector2f(0, -5 * mult));
 
 	if(game->getInputHandler().keyPressed(sf::Keyboard::S))
-		player1.move(sf::Vector2f(0, 5 * game->getTimeStep().getMultiplier()));
+		player1.move(sf::Vector2f(0, 5 * mult));
 
 	if(game->getInputHandler().keyPressed(sf::Keyboard::Up))
-		player2.move(sf::Vector2f(0, -5 * game->getTimeStep().getMultiplier()));
+		player2.move(sf::Vector2f(0, -5 * mult));
 	
 	if(game->getInputHandler().keyPressed(sf::Keyboard::Down))
-		player2.move(sf::Vector2f(0, 5 * game->getTimeStep().getMultiplier()));
+		player2.move(sf::Vector2f(0, 5 * mult));
 
 	if(ball.getPosition().y + ball.getRadius() >= 500 || ball.getPosition().y <= 0)
 		ball_velocity_y *= -1;
 	if(ball.getPosition().x + ball.getRadius() >= 500 || ball.getPosition().x <= 0)
 		ball_velocity_x *= -1;
 
-	ball.move(sf::Vector2f(ball_velocity_x, ball_velocity_y));
+	ball.move(sf::Vector2f(ball_velocity_x * mult, ball_velocity_y * mult));
 
 }
 
--- Examples/Pong/Pong.hpp
+++ Examples/Pong/Pong.hpp
@@ -1,14 +1,22 @@
 #ifndef PONG_HEADER
 #define PONG_HEADER
 
+// Include the CGFW header which includes all of the necessary headers
 #include "CGFW.hpp"
 
+/*
+State classes, from entire games to option menus, must extend the game
+state class that contains two virtual functions tick() and draw().
+*/
 class Pong : public cgfw::GameState {
 
 public:
 	Pong();
 
+	// Logic that is calculated here
 	virtual void tick(cgfw::Game* game);
+
+	// Any rendering is done here.
 	virtual void render(sf::RenderWindow* window);
 
 private:
